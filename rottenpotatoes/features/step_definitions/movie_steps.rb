f98a3918a37a00be008416eea5364b8236a93939
rbrown26 <rbrown26@citadel.edu>
2018-10-16 00:57:40 +0000

    part 3 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -6,7 +6,6 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
   end
-  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -19,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  regexp = /#{e1}.*#{e2}/m
+  page.body.should =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
